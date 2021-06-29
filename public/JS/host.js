8d2a8ddfafa94f552735672ed9069077cc03ae92
roatt-tilma <roshanbhattmnr@gmail.com>
2021-06-30 00:17:23 +0545

    added turn server

--- public/JS/host.js
+++ public/JS/host.js
@@ -85,23 +85,13 @@ socket.on('answer', (answer) => {
 function createPeer(){
     return new RTCPeerConnection({
         iceServers: [
-            {
-                urls: "stun:stun.stunprotocol.org:3478"
+            { 
+                url: "stun:stun.l.google.com:19302" 
             },
             {
-                urls: "stun:stun.l.google.com:19302"
-            },
-            {
-                urls: "stun:stun1.l.google.com:19302"
-            },
-            {
-                urls: "stun:stun2.l.google.com:19302"
-            },
-            {
-                urls: "stun:stun3.l.google.com:19302"
-            },
-            {
-                urls: "stun:stun4.l.google.com:19302"
+                url: "turn:numb.viagenie.ca",
+                credential: "I1server",
+                username: "roarout20@gmail.com",
             }
         ]
     });
