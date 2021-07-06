bebec99b366b8eec214dff664d12318470461b2b
roatt-tilma <roshanbhattmnr@gmail.com>
2021-07-06 12:26:36 +0545

    removed turnservers

--- public/JS/room.js
+++ public/JS/room.js
@@ -406,21 +406,6 @@ function createPeer(){
             {urls:'stun:stun.voipstunt.com'},
             {urls:'stun:stun.voxgratia.org'},
             {urls:'stun:stun.xten.com'},
-            {
-                urls: 'turn:numb.viagenie.ca',
-                credential: 'muazkh',
-                username: 'webrtc@live.com'
-            },
-            {
-                urls: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-            {
-                urls: 'turn:192.158.29.39:3478?transport=tcp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            }
         ]
     });
 
